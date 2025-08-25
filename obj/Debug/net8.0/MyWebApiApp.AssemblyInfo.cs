e72bba23f2fa8322475a50f1dbdeac9c38ab0adb
Ayush Solanki <ayushsolanki1403@gmail.com>
2025-08-25 09:01:02 +0530

    First Commit

--- obj/Debug/net8.0/MyWebApiApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyWebApiApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyWebApiApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df9945c0746a6212d16deca7348c78fa21247cfe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a36fb1c8731b846be433750ac88fed2d463101a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyWebApiApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyWebApiApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
